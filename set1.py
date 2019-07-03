95a32bdca8471712d1fd6b895a5176d03e618879
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-03 09:37:31 +0530

    Update set1.py

--- set1.py
+++ set1.py
@@ -1,7 +1,7 @@
 num=float(input("enter num:")
 if num>0:
-print("the num is positive ")
+          print("the num is positive ")
 elif num==0:
-print("zero")
-else
-print("the num is negative")
+          print("zero")
+else:
+          print("the num is negative")
