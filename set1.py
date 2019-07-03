c36c4bee79a0e087bf548d802dfe92b103c0ca1c
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-03 09:55:40 +0530

    Update set1.py

--- set1.py
+++ set1.py
@@ -1,7 +1,7 @@
 num=int(input())
 if num>0:
-          print(" positive ")
+          print(" Positive ")
 elif num<0:
-          print("negative")
+          print("Negative")
 else num==0:
           print("zero")
